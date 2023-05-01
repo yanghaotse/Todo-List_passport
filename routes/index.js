8df14a09f6be07937c71745bd4bf24f26bc0b530
yang haotse <ha91252@gmail.com>
2023-05-01 13:10:06 +0800

    feat: add users modules

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const home = require('./models/home')
-const todos = require('./models/todos')
+const home = require('./modules/home')
+const todos = require('./modules/todos')
 
+const users = require('./modules/users')
 
 // 將網址結構符合 / 字串的 request 導向 home 模組
 router.use('/', home)
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
 router.use('/todos', todos)
+
+router.use('/users', users)
 
 
 
