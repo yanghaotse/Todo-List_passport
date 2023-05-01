1350b5818602cdc21fc83f694d9842beb69d81df
yang haotse <ha91252@gmail.com>
2023-05-01 21:25:25 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
-// const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-// const Todo = require('./models/todo') //載入Todo model
 const bodyParser = require('body-parser')
-
 const methodOverride = require('method-override')
 const routes = require('./routes')//引入路由器時，路徑設定為 /routes 就會自動去尋找目錄下叫做 index 的檔案
 require('./config/mongoose')
+const session = require('express-session')
+
+
 const PORT = process.env.PORT || 3000
 const app = express()
 
@@ -16,7 +16,13 @@ app.engine('hbs', exphbs({defaultLayout : 'main', extname : '.hbs'}))//在應用
 app.set('view engine', 'hbs')//hbs元件 正式掛載
 app.use(express.urlencoded({ extended: true}))//body-parser
 app.use(methodOverride('_method'))
-
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false, 
+  //resave: 當設定為 true 時，會在每一次與使用者互動後，強制把 session 更新到 session store 裡。
+  saveUninitialized: true
+  //saveUninitialized: 強制將未初始化的 session 存回 session store。未初始化表示這個 session 是新的而且沒有被修改過，例如未登入的使用者的 session。
+}))
 
 
 app.use(routes)
